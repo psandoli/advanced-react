2e28514637c6d0849aa04734a27f211de1e310e1
Paula Sandoli <paulasandoli@gmail.com>
2022-07-18 13:36:08 -0300

    Configure eslint and add dependecies

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "parser": 'babel-eslint',
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true,
+        "node: true"
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true,
+            "experimentalObjectRestSpread": true.
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/prop-types":  ["off"],
+        "indent": ["error", 2],
+        "linebreack-style": ["error", "unix"],
+        "quotes": ["error", "single"],
+        "no-console": ["warn", { "allow": ["info", "error"]}]
+    }
+}
